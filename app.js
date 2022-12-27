ae02bb2e066ef2e87d498e50b0da0c2fedca30cd
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-27 22:04:50 +0630

    connect to db

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require('./db/connectDB');
+const notFound = require('./middleware/notFound');
 
+connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
 
 // Routes
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/blogs', require('./routes/blog'));
+
+app.use(notFound);
 
 app.listen(port, () => console.log(`Server listing on port ${port}`));
